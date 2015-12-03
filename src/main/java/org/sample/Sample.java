2752830ed85ce8af2fcba25405e87fd1f280265d
towieger <thomas.wieger@t-systems.com>
2015-12-03 16:51:47 +0100

    added simple service

---
+++ src/main/java/org/sample/Sample.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Sample {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
