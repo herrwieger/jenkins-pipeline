2be58af484ede7da9e0ba072c9994e331f81ba3c
towieger <thomas.wieger@t-systems.com>
2017-03-08 13:14:10 +0100

    added jenkinsfile with pipeline definition

---
+++ jenkinsfile.groovy
@@ -0,0 +1,18 @@
+node {
+   def mvnHome
+   stage('Preparation') { // for display purposes
+      // Get some code from a GitHub repository
+      git 'https://github.com/herrwieger/springbootsample.git'
+      // Get the Maven tool.
+      // ** NOTE: This 'M3' Maven tool must be configured
+      // **       in the global configuration.
+      mvnHome = tool 'M3'
+   }
+   stage('Build') {
+         bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+   }
+   stage('Results') {
+      junit '**/target/surefire-reports/TEST-*.xml'
+      archive 'target/*.jar'
+   }
+}
